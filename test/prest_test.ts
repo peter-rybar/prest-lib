8b43c9d815eb1494341e425eced309cb207417ee
Peter Rybar <pr.rybar@gmail.com>
2016-05-17 14:22:01 +0200

    prest.http quieck functions added (GET, POST, PUT, DELETE)

--- test/prest_test.ts
+++ test/prest_test.ts
@@ -103,7 +103,7 @@ function main() {
 
     new prest.http.HttpRequest()
         .url('https://maps.googleapis.com/maps/api/geocode/json',
-             {sensor: false, address: 'Bratislava', xxx: ['yyy', 'zzz']})
+             {sensor: false, address: 'Bratislava I', xxx: ['yyy', 'zzz']})
         .method('GET')
         .onResponse((response:prest.http.HttpResponse) => {
             console.log("response: " + response.getContentType(), response.getJson());
@@ -112,4 +112,15 @@ function main() {
             console.log("response error: ", error);
         })
         .send();
+
+    prest.http.GET(
+        'https://maps.googleapis.com/maps/api/geocode/json',
+        {sensor: false, address: 'Bratislava II', xxx: ['yyy', 'zzz']},
+        (err, res) => {
+            if (err) {
+                console.log("response error: ", err);
+            } else {
+                console.log("response: " + res.getContentType(), res.getJson());
+            }
+        });
 }
