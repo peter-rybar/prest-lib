0f1d1dcda7f06814b4239c423b462df4f1e6e03a
Peter Rybar <pr.rybar@gmail.com>
2016-06-18 23:44:26 +0200

    history module implemented

---
+++ src/prest/prest-history.ts
@@ -0,0 +1,41 @@
+module prest.history {
+
+    export class History<T> {
+
+        back(distance?:any) {
+            window.history.back(distance);
+        }
+
+        forward(distance?:any) {
+            window.history.forward(distance);
+        }
+
+        go(delta?:any) {
+            window.history.go(delta);
+        }
+
+        length():number {
+            return window.history.length;
+        }
+
+        state():any {
+            return window.history.state;
+        }
+
+        pushState(state:T, title:string, url?:string) {
+            window.history.pushState(state, title, url);
+        }
+
+        replaceState(state, title:string, url?:string) {
+            window.history.replaceState(state, title, url);
+        }
+
+        onChange(callback:(state:T)=>void) {
+            window.addEventListener('popstate', function (e:PopStateEvent) {
+                callback(e.state);
+            });
+        }
+
+    }
+
+}
