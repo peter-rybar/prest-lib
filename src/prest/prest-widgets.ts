f446d5c0286a25f1a9d9c39bbff000943916494b
Peter Rybar <pr.rybar@gmail.com>
2016-08-20 11:44:45 +0200

    widget module improved

--- src/prest/prest-widgets.ts
+++ src/prest/prest-widgets.ts
@@ -5,65 +5,59 @@ module prest.widgets {
     }
 
 
-    export abstract class XWidget implements Widget {
+    export function element(html:string):HTMLElement {
+        var e:HTMLElement = document.createElement('div');
+        e.innerHTML = html.trim();
+        var f:DocumentFragment = document.createDocumentFragment();
+        return <HTMLElement>f.appendChild(e.removeChild(e.firstChild));
+    }
 
-        static element(html:string):HTMLElement {
-            var d:HTMLElement = document.createElement('div');
-            d.innerHTML = html.trim();
-            var f:DocumentFragment = document.createDocumentFragment();
-            return f.appendChild(d.removeChild(d.firstChild)) as HTMLElement;
+    export function empty(element:HTMLElement) {
+        while (element.firstChild /*.hasChildNodes()*/) {
+            element.removeChild(element.firstChild);
         }
+    }
 
-        static xEventsBind(element, context, prefix='x-') {
-            var events = [];
-            for (var k in context) {
-                var m = k.match(/^_([a-z]+)_.*/);
-                if (m && events.indexOf(m[1]) == -1) {
-                    events.push(m[1]);
-                }
-            }
-            // for (var k in element) {
-            //     if (k.search('on') === 0) {
-            //         events.push(k.slice(2));
+    export function xEventsBind(element:HTMLElement, context:Object, events:Array<string>=[]) {
+        // var events = [];
+        if (!events.length) {
+            // for (var k in context) {
+            //     var m = k.match(/^_([a-z]+)_.*/);
+            //     if (m && events.indexOf(m[1]) == -1) {
+            //         events.push(m[1]);
             //     }
             // }
-            // console.log(events);
-            for (var eventType of events) {
-                var x = element.querySelectorAll('[' + prefix + eventType + ']');
-                // console.log('xEventsBind: ', eventType, x);
-                if (x.length) {
-                    // console.debug('xEventsBind: ', prefix + eventType);
-                    element.addEventListener(eventType, function (e) {
-                        e.stopPropagation();
-                        var event = e || window.event;
-                        var target = (event.target || e.srcElement) as HTMLElement;
-                        if (target.hasAttribute(prefix + event.type)) {
-                            var methodName = target.getAttribute(prefix + event.type);
-                            var method = context[methodName];
-                            // console.debug('xEventsBind call: ', event.type, methodName, method);
-                            if (method) {
-                                method.call(context, target, event);
-                            } else {
-                                console.warn('xEventsBind method mising: ', methodName);
-                            }
-                        }
-                        return false;
-                    })
+            for (var k in element) {
+                if (k.search('on') === 0) {
+                    var event = k.slice(2);
+                    var x = element.querySelectorAll('[x-' + event + ']');
+                    // console.log('xEventsBind: ', event, x);
+                    if (x.length) {
+                        events.push(event);
+                    }
                 }
             }
         }
-
-        protected _element:HTMLElement;
-
-        element():HTMLElement {
-            if (!this._element) {
-                this._element = this._render();
-            }
-            return this._element;
+        // console.log(events);
+        for (var event of events) {
+            // console.debug('xEventsBind: ', 'x-' + event);
+            element.addEventListener(event, function (e) {
+                e.stopPropagation();
+                var evt = e || window.event;
+                var target = (evt.target || e.srcElement) as HTMLElement;
+                if (target.hasAttribute('x-' + evt.type)) {
+                    var methodName = target.getAttribute('x-' + evt.type);
+                    var method = context[methodName];
+                    // console.debug('xEventsBind call: ', evt.type, methodName, method);
+                    if (method) {
+                        method.call(context, target, evt);
+                    } else {
+                        console.warn('xEventsBind mising method: ', methodName);
+                    }
+                }
+                return false;
+            })
         }
-
-        protected abstract _render():HTMLElement;
-
     }
 
 }
