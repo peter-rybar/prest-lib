23a77db91d9aec66a6703e2c9664fe825f4808f1
Peter Rybar <pr.rybar@gmail.com>
2016-09-13 21:05:44 +0200

    element form html use template otherwise fallback to wrapper

--- src/prest/prest-widgets.ts
+++ src/prest/prest-widgets.ts
@@ -6,10 +6,56 @@ namespace prest.widgets {
 
 
     export function element(html: string): HTMLElement {
-        const e: HTMLElement = document.createElement("div");
-        e.innerHTML = html.trim();
-        const f: DocumentFragment = document.createDocumentFragment();
-        return <HTMLElement>f.appendChild(e.removeChild(e.firstChild));
+        const t = document.createElement("template") as HTMLTemplateElement;
+        if ("content" in t) {
+            t.innerHTML = html;
+            return t.content.cloneNode(true) as HTMLElement;
+        } else {
+            let wrapMap: any = {
+                option: [1, "<select multiple='multiple'>", "</select>"],
+                legend: [1, "<fieldset>", "</fieldset>"],
+                area: [1, "<map>", "</map>"],
+                param: [1, "<object>", "</object>"],
+                thead: [1, "<table>", "</table>"],
+                tr: [2, "<table><tbody>", "</tbody></table>"],
+                col: [2, "<table><tbody></tbody><colgroup>", "</colgroup></table>"],
+                td: [3, "<table><tbody><tr>", "</tr></tbody></table>"],
+                body: [0, "", ""],
+                _default: [1, "<div>", "</div>"]
+            };
+            wrapMap.optgroup = wrapMap.option;
+            wrapMap.tbody = wrapMap.tfoot = wrapMap.colgroup = wrapMap.caption = wrapMap.thead;
+            wrapMap.th = wrapMap.td;
+            const match = /<\s*\w.*?>/g.exec(html);
+            let element: HTMLElement = document.createElement("div");
+            if (match != null) {
+                const tag = match[0].replace(/</g, "").replace(/>/g, "").split(" ")[0];
+                if (tag.toLowerCase() === "body") {
+                    const body = document.createElement("body");
+                    // keeping the attributes
+                    element.innerHTML = html.replace(/<body/g, "<div").replace(/<\/body>/g, "</div>");
+                    const attrs = element.firstChild.attributes;
+                    body.innerHTML = html;
+                    for (let i = 0; i < attrs.length; i++) {
+                        body.setAttribute(attrs[i].name, attrs[i].value);
+                    }
+                    return body;
+                } else {
+                    const map = wrapMap[tag] || wrapMap._default;
+                    html = map[1] + html + map[2];
+                    element.innerHTML = html;
+                    // Descend through wrappers to the right content
+                    let j = map[0] + 1;
+                    while (j--) {
+                        element = element.lastChild as HTMLElement;
+                    }
+                }
+            } else {
+                element.innerHTML = html;
+                element = element.lastChild as HTMLElement;
+            }
+            return element;
+        }
     }
 
     export function empty(element: HTMLElement) {
