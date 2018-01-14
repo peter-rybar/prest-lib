9111c39b60c9709019427ac7e73b22611b6374be
Peter Rybar <pr.rybar@gmail.com>
2018-01-14 01:35:32 +0100

    client router based on url hash

---
+++ src/demo/router_demo.ts
@@ -0,0 +1,33 @@
+
+import { Router } from "../main/prest/router";
+
+function print(...args: any[]) {
+    console.log.apply(console, args);
+    const out = document.getElementById("output");
+    out.innerHTML += "<br>" + args.join(" ");
+}
+
+// #users/123
+Router.route("users/:id", (id: string) => {
+    print("users/:id |", id);
+});
+// #users/123/account/321
+Router.route("users/:uid/account/:aid", (uid: string, aid: string) => {
+    print("users/123/account/321 |", uid, aid);
+});
+// #users
+Router.route("users", (path: string) => {
+    print("users |", path);
+});
+// #users/foo/bar
+Router.route("users/*", (path: string) => {
+    print("users/* |", path);
+});
+// #all/paths
+Router.route("*", (path: string) => {
+    print("* |", path);
+});
+
+Router.start();
+
+Router.navigate("users/987");
