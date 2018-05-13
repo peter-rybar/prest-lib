2e60f6e5eb87115a3658f43335d495672f44c150
Peter Rybar <pr.rybar@gmail.com>
2018-05-13 11:37:05 +0200

    events - handling multiple events

--- src/demo/events_demo.ts
+++ src/demo/events_demo.ts
@@ -1,9 +1,8 @@
 import { Events } from "../main/events";
 
-
 const es = new Events<number>(3);
 
-es.on(undefined, (data, ctx, e) => console.log("on all:", data, ctx, e));
+es.any((data, ctx, e) => console.log("any:", data, ctx, e));
 
 es.emit("e", "eee1");
 es.on("e", (data, ctx, e) => console.log(data, ctx, e));
@@ -11,11 +10,16 @@ es.emit("e", "eee2");
 es.off("e");
 es.emit("e", "eee3");
 
-es.off(undefined);
+es.off(Events.any);
 
-es.once(undefined, (data, ctx, e) => console.log("once all:", data, ctx, e));
+es.once(Events.any, (data, ctx, e) => console.log("once all:", data, ctx, e));
 
 es.emit("o", "ooo1");
 es.once("o", (data, ctx, e) => console.log(data, ctx, e));
 es.emit("o", "ooo2");
 es.emit("o", "ooo3");
+
+es.all(["e1", "e3"], (data, ctx, e) => console.log(data, ctx, e));
+es.emit("e1", "all e1");
+es.emit("e2", "all e2");
+es.emit("e3", "all e3");
--- src/main/events.ts
+++ src/main/events.ts
@@ -1,6 +1,8 @@
 
 export class Events<C> {
 
+    static any = "";
+
     private _ctx: C;
     private _cbs: { [e: string]: Array<(data: any, ctx: C, e: string) => void> };
     private _cb: Array<(data: any, ctx: C, e: string) => void>;
@@ -23,6 +25,16 @@ export class Events<C> {
         if (this._cbs[e].indexOf(cb) === -1) {
             this._cbs[e].push(cb);
         }
+        return this;
+    }
+
+    any(cb: (data: any, ctx: C, e: string) => void): this {
+        this.on(Events.any, cb);
+        return this;
+    }
+
+    all(es: string[], cb: (data: any, ctx: C, e: string) => void): this {
+        es.forEach(e => this.on(e, cb));
         return this;
     }
 
@@ -71,9 +83,10 @@ export class Events<C> {
 
 }
 
+
 // const es = new Events<number>(3);
 
-// es.on(undefined, (data, ctx, e) => console.log("on all:", data, ctx, e));
+// es.any((data, ctx, e) => console.log("any:", data, ctx, e));
 
 // es.emit("e", "eee1");
 // es.on("e", (data, ctx, e) => console.log(data, ctx, e));
@@ -81,11 +94,16 @@ export class Events<C> {
 // es.off("e");
 // es.emit("e", "eee3");
 
-// es.off(undefined);
+// es.off(Events.any);
 
-// es.once(undefined, (data, ctx, e) => console.log("once all:", data, ctx, e));
+// es.once(Events.any, (data, ctx, e) => console.log("once all:", data, ctx, e));
 
 // es.emit("o", "ooo1");
 // es.once("o", (data, ctx, e) => console.log(data, ctx, e));
 // es.emit("o", "ooo2");
 // es.emit("o", "ooo3");
+
+// es.all(["e1", "e3"], (data, ctx, e) => console.log(data, ctx, e));
+// es.emit("e1", "all e1");
+// es.emit("e2", "all e2");
+// es.emit("e3", "all e3");
